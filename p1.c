c58f2637e9718c6a70abdfd26f6c954bb40fae75
Sajjad-arch <sajjadulislam870@gmail.com>
2024-12-05 23:14:11 +0600

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+int main(void)
+{
+    printf("Max value of int: %d\n", INT_MAX);
+    printf("Max value +1 of int: %d\n", INT_MAX + 1);
+    printf ("Min value of int: %d\n", INT_MIN);
+    printf("Min value 1 of int: %d\n", INT_MIN - 1);
+return 0;
+}
